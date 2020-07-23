5ebc030309ca5cb98720e8392f6d883398093cd7
Igor Ribozavr <Igo-nikolaenk@yandex.ru>
2020-07-23 14:37:08 +0300

    Models added

---
+++ models/goodsExist.js
@@ -0,0 +1,16 @@
+// Это товары, которые существуют у нас в смузийной.
+// При вводе кода товара мы будем проверять его наличие.
+// На следующем шаге в массив юзера будет добавляться товар.
+// Поля  title и keywords будут копироваться в goods и уже этот чувак у которого будет уже дата его добавления лететь в массив юзера.
+
+const mongoose = require('mongoose');
+const { Schema, model } = require('mongoose');
+
+const schemaGoodsExist = new Schema({
+  title: String,
+  keywords: String,
+  dateCreate: { type: Date, default: Date.now },
+});
+
+const GoodExist = model('GoodsExist', schemaGoodsExist);
+module.exports = GoodExist;
