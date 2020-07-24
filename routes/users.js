a3d11dcc0b14cd5dcbf84c92044dea5f17cbe882
MarselBurdo <9003363@gmail.com>
2020-07-24 11:36:19 +0300

    notes correct, seed

--- routes/users.js
+++ routes/users.js
@@ -1,29 +1,21 @@
-const router = require('express').Router()
+const router = require('express').Router();
 // const error = require('../middleware/error');
-const {checkSession, checkVerification, cookiesCleaner} = require('../middleware/check')
+const { checkSession, checkVerification, cookiesCleaner } = require('../middleware/check');
+const note = require('../models/note');
 
-router.get('/', (req, res)=>{
+router.get('/', (req, res) => {
   res.redirect('/users/home');
-})
+});
 
 router
   .route('/home')
-  .get(checkSession,(req,res)=>{
-    res.render('home')
-  })
-  // .get((req,res)=>{
-  //   const {user} = req.session
-  //   if (!req.session.user) {
-  //     res.render('home', { user: user.username })
-      
-  //   } else {
-  //     res.redirect('/login');
-  //   }
-  // })
+  .get((req, res) => {
+    res.render('home', { user: 'Ivan' });
+  });
 
 router
   .route('/logouts')
-  .get(checkSession,async (req, res, next) => {
+  .get(checkSession, async (req, res, next) => {
     if (req.session.user) {
       try {
         await req.session.destroy();
@@ -35,12 +27,19 @@ router
     } else {
       res.redirect('/');
     }
+  });
+
+router
+  .route('/note')
+  .get(async (req, res) => {
+    const notes = await note.find({}).limit(15);
+    console.log(notes);
+    res.render('note', {notes});
+  });
+router
+  .route('/note/new')
+  .get((req,res)=>{
+    res.render('createnote')
   })
 
-  router
-    .route('/note')
-    .get((req,res)=> {
-      res.render('note')
-    })
-
-module.exports = router
+module.exports = router;
