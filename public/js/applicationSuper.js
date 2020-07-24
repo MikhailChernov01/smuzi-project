61fd32263b7f02ab68dd997139417dd46d1d3c07
Станислав Веселов <61013428+BearablePain@users.noreply.github.com>
2020-07-24 16:09:01 +0300

    Spisok Users

--- public/js/applicationSuper.js
+++ public/js/applicationSuper.js
@@ -1,24 +1,21 @@
-const buttonMounth = document.querySelector('#btn-mounth')
-const buttonWeek = document.querySelector('#btn-week')
-const buttonThreDays = document.querySelector('#btn-three-day')
-
+const buttonMounth = document.querySelector('#btn-mounth');
+const buttonWeek = document.querySelector('#btn-week');
+const buttonThreDays = document.querySelector('#btn-three-day');
+const customer = document.querySelector('#customers');
+let body = document.querySelector('body')
 
 async function generateDashBoard(data) {
-
-
-
-
   const responce = await fetch(`super/${data}`);
   const result = await responce.json();
-  let regData = result.sortData.map(
-    (el) => el.match(/(\d{2})\s(\d{4})/g).join('')
-    // \s((\d{2}):){2}(\d{2})
-  );
+  // let regData = result.sortData.map(
+  //   (el) => el.match(/(\d{2})\s(\d{4})/g).join('')
+  // \s((\d{2}):){2}(\d{2})
+  // );
   let ctx = document.getElementById('myChart');
   let myChart = new Chart(ctx, {
     type: 'line',
     data: {
-      labels: regData,
+      labels: result.sortData,
       datasets: [
         {
           data: result.arrSum,
@@ -46,15 +43,28 @@ async function generateDashBoard(data) {
     },
   });
 }
-generateDashBoard('week')
+generateDashBoard('week');
 
 buttonMounth.addEventListener('click', async () => {
-  generateDashBoard('mounth')
-})
+  generateDashBoard('mounth');
+});
+
 buttonWeek.addEventListener('click', async () => {
-  generateDashBoard('week')
-})
+  generateDashBoard('week');
+});
 
 buttonThreDays.addEventListener('click', async () => {
-  generateDashBoard('threeDays')
-})
+  generateDashBoard('threeDays');
+});
+
+
+customer.addEventListener('click', async (e) => {
+e.preventDefault()
+ 
+ const responce = await fetch('super/customers')
+ let res = await responce.text()
+//  res = Handlebars.compile(res)
+ body.innerHTML = res;
+// console.log(res)
+// body.append(res)
+});
