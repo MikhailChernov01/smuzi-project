5db792a69dec18498cae324ea8c8ae7b4eb252ef
MarselBurdo <9003363@gmail.com>
2020-07-25 22:48:12 +0300

    some commit

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,9 @@
+/* eslint-disable no-underscore-dangle */
 const router = require('express').Router();
 // const error = require('../middleware/error');
 const { checkSession, checkVerification, cookiesCleaner } = require('../middleware/check');
 const Note = require('../models/note');
-
+const User = require('../models/users');
 
 router.get('/', (req, res) => {
   res.redirect('/users/home');
@@ -10,7 +11,7 @@ router.get('/', (req, res) => {
 
 router
   .route('/home')
-  .get((req, res) => {
+  .get(checkSession, (req, res) => {
     const { user } = req.session;
     if (req.session.user) {
       res.render('home');
@@ -39,7 +40,6 @@ router
   .route('/note')
   .get(async (req, res) => {
     const notes = await Note.find({}).limit(15);
-    console.log(notes);
     res.render('note', { notes });
   });
 
@@ -51,6 +51,36 @@ router
 
 router
   .route('/bottle')
-  .post
+  .post(checkSession, async (req, res) => {
+    const { user } = req.session;
+    const goods = req.body;
+    await User.updateOne({ _id: user._id }, { $push: { purchases: goods.inputNewCode } });
+    res.redirect('/users/home');
+  });
+
+router
+  .route('/note/:id')
+  .get(async (req, res) => {
+    let note = await Note.findById(req.params.id)
+    // console.log(autor);
+    let test = req.session.user._id
+    console.log(test);
+    let newNote = await Note.findByIdAndUpdate({_id:req.params.id},{author: test}, {new: true});
+    // console.log(test);
+    // note.title = test;
+    // console.log(note);
+
+    // req.session.user._id
+    // try {
+    //   if((!note.author===undefined)&&(String(note.author)===String(user._id))) {
+
+    //     console.log(user._id);
+    //     console.log(note);
+    //   }
+    // } catch (error) {
+    //   next(error)
+    // }
+
+  });
 
 module.exports = router;
